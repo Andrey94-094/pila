146b31623807f91eaedcc0b742bf7d7966e7d3a2
Andrey94-094 <andrey940427@gmail.com>
2021-12-24 13:57:12 +0300

    pilav2

--- src/js/components/smoothScroll.js
+++ src/js/components/smoothScroll.js
@@ -1,11 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.smoothscroll').forEach(item => {
-        item.addEventListener('click', (event) => {
-            event.preventDefault();
-            window.scrollTo({
-                top: document.getElementById(item.getAttribute('href')).offsetTop,
-                behavior: "smooth"
-            });
-        });
+let _config = {
+    selector: 'a[href^="#"]',
+    offsetTop: 0
+}
+
+export default (config = {}) => {
+    _config = {..._config, ...config }
+    Array.prototype.forEach.call(document.querySelectorAll(_config.selector), el => {
+        el.addEventListener('click', ev => {
+            if (typeof el.hash !== "undefined" && document.querySelector(el.hash) !== null) {
+                ev.preventDefault()
+                let scrollOptions = { left: 0, top: 0, behavior: 'smooth' }
+                scrollOptions.top = document.querySelector(el.hash).offsetTop - _config.offsetTop;
+                if ('scrollBehavior' in document.documentElement.style) {
+                    window.scrollTo(scrollOptions)
+                } else {
+                    window.scrollTo(0, scrollOptions.top)
+                }
+            }
+        })
     })
-});
+}
--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,13 @@ import '../../node_modules/slick-carousel/slick/slick';
 import '../../node_modules/focus-visible/dist/focus-visible';
 import '../sass/style.scss';
 
-import './components/smoothScroll';
+import smoothScroll from './components/smoothScroll';
 import './components/formSubmit';
 import './components/header';
 import './components/countdown';
 import './components/test';
 import './components/slider';
+
+document.addEventListener('DOMContentLoaded', () => {
+    smoothScroll()
+})
